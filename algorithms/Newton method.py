70d6515df64c332e99cb1bfc5e8f4d9cce1136e5
Dutkowska <dutkowska@yahoo.com>
2018-12-23 17:18:41 +0100

    adding newton method

---
+++ algorithms/Newton method.py
@@ -0,0 +1,22 @@
+from math import *
+from sympy import *
+import numpy as np
+def newton_method(f, df, x0, eps, eps2):
+    #f(x)=0, |f(x)|<eps, eps2 - max iterations
+    xn=x0
+    for i in range(eps2):
+        fn=f(xn)
+        if abs(fn)<eps:
+            print("Iterations: ", i, "; solution:")
+            return xn
+        
+        Dfn=Df(xn)
+        if Dfn == 0:
+            print("No solutions")
+        xn=xn-fn/Dfn
+
+fx=input("Enter a polynomian f, such as f(x)=0: ")
+f=lambda x: eval(fx)
+df=lambda x: np.diff(f)
+x0=int(input("Enter a start point: "))
+print(newton_method(f,df,x0,0.0001,10))
