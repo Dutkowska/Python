8e49dca8f90c9bc75333fb88d6c03836c7d51960
Dutkowska <dutkowska@yahoo.com>
2019-02-02 00:47:18 +0100

    adding spoj files

---
+++ spoj/binoms-Dwumiany.py
@@ -0,0 +1,9 @@
+def bin_coe(n, k):
+    if k==0 or k==n: return 1
+    elif n<k: return "Wrong numbers"
+    else: return bin_coe(n-1, k-1)+bin_coe(n-1, k)
+t=int(input())
+for i in range(t):
+    tab=[int(x) for x in input().split()]
+    n, k = tab[0], tab[1]
+    print(bin_coe(n,k))
